fce4520790fdd03a1ae6f39c3d5f042c757e9efa
inuoshios <inuoshios@gmail.com>
2022-11-09 06:45:16 +0100

    init: initialized git repo

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "price-action",
+	Short: "Price update of cryto and stock market",
+	Long: `A price update of different crypto currencies
+and different coins`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Price Action")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
