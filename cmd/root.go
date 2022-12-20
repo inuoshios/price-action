2f7e40c3609ad19a9f28e65310c01d0f19c26189
Inu John <inuoshios@gmail.com>
2022-12-20 18:43:08 +0100

    fix: fixed go imports

--- cmd/root.go
+++ cmd/root.go
@@ -6,8 +6,8 @@ package cmd
 import (
 	"os"
 
-	"github.com/ixxiv/price-action/utils/theme"
 	f "github.com/mbndr/figlet4go"
+	"github.com/rxxcc/price-action/utils/theme"
 	"github.com/spf13/cobra"
 )
 
